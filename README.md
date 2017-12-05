76f728257bb9020b8647b41542e001b97bf81824
Ivan Ivanov <i.ivanov@teracomm.bg>
2017-12-05 16:27:46 +0200

    Implement log() and error() methods

--- README.md
+++ README.md
@@ -16,10 +16,22 @@ var debug = require('node-debug');
 
 ### log()
 
-Return current timestamp (milliseconds since 1970/01/01)
+Logs the message as if you called console.log but prefixes the output with the process.pid and current time
 
 ``` js
-debug.log('APP', 'Starting server');
+debug.log('APP', 'Starting server', port);
+
+// [5635] [2017-12-05T13:21:07.558Z] [APP] [Starting server] [80]
+```
+
+### error()
+
+Logs the message as if you called console.error but prefixes the output with the process.pid and current time
+
+``` js
+debug.error('APP', 'Server error', error);
+
+// [5635] [2017-12-05T13:21:07.558Z] [APP] [Server error] [Port already in use]
 ```
 
 ## License
---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var self = module.exports = {
+    /**
+     * Logs the message as if you called console.log but prefixes the output with the process.pid and current time
+     */
+    log: function() {
+        let args = Array.prototype.slice.call(arguments);
+
+        args.unshift(new Date().toISOString());
+        args.unshift(process.pid);
+
+        args = args.map(i => '[' + i + ']');
+
+        console.log.apply(this, args);
+    },
+
+    /**
+     * Logs the message as if you called console.error but prefixes the output with the process.pid and current time
+     */
+    error: function() {
+        let args = Array.prototype.slice.call(arguments);
+
+        args.unshift(new Date().toISOString());
+        args.unshift(process.pid);
+
+        args = args.map(i => '[' + i + ']');
+
+        console.error.apply(this, args);
+    }
+};
